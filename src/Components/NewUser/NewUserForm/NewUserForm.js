ef3d44f2821455e852c67b1c0ef215ed51d437a3
Abishek Balaji <abishekbalaji7.151@gmail.com>
2022-11-19 21:04:12 +0530

    Added ref instead of state in user form

--- src/Components/NewUser/NewUserForm/NewUserForm.js
+++ src/Components/NewUser/NewUserForm/NewUserForm.js
@@ -1,27 +1,29 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Modal from "../../Modal/Modal";
 import Button from "../../UI/Button/Button";
 import styles from "./NewUserForm.module.css";
 
 const NewUserForm = function ({ onCollectUser }) {
-  const [nameInput, setNameInput] = useState("");
-  const [ageInput, setAgeInput] = useState("");
+  // const [nameInput, setNameInput] = useState("");
+  // const [ageInput, setAgeInput] = useState("");
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
   const [modalVisibility, setModalVisibility] = useState(false);
   const [modalData, setModalData] = useState("");
 
-  const nameChangeHandler = (e) => {
-    setNameInput(e.target.value);
-  };
+  // const nameChangeHandler = (e) => {
+  //   setNameInput(e.target.value);
+  // };
 
-  const ageChangeHandler = (e) => {
-    setAgeInput(e.target.value);
-  };
+  // const ageChangeHandler = (e) => {
+  //   setAgeInput(e.target.value);
+  // };
 
   const submitHandler = (e) => {
     e.preventDefault();
     const userData = {
-      name: nameInput,
-      age: ageInput,
+      name: nameInputRef.current.value,
+      age: ageInputRef.current.value,
     };
     if (userData.name.trim() === "" || userData.age.trim() === "") {
       setModalVisibility(true);
@@ -34,8 +36,10 @@ const NewUserForm = function ({ onCollectUser }) {
       return;
     }
     onCollectUser(userData);
-    setNameInput("");
-    setAgeInput("");
+    // Below way of manipulating DOM directly is not advised.
+    // refs have direct access to their DOM elements.
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
   };
 
   const modalCloseHandler = () => {
@@ -48,11 +52,11 @@ const NewUserForm = function ({ onCollectUser }) {
         <div className={styles["new-user__controls"]}>
           <div className={styles["new-user__control"]}>
             <label>Username</label>
-            <input value={nameInput} onChange={nameChangeHandler} type="text" />
+            <input ref={nameInputRef} type="text" />
           </div>
           <div className={styles["new-user__control"]}>
             <label>Age (Years)</label>
-            <input value={ageInput} onChange={ageChangeHandler} type="number" />
+            <input ref={ageInputRef} type="number" />
           </div>
         </div>
         <div>
